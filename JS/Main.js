a45ea722540ec6e16b297cdb96520d86f20f7d3a
Ali Bidkhori <a.bidkhori2004@gmail.com>
2021-10-03 12:30:43 +0200

    1.0 Update - added First HTML, CSS, JS codes and some fonts and images

---
+++ JS/Main.js
@@ -0,0 +1,41 @@
+function onload()
+{
+    var typed = new Typed('#typing', {
+        strings:
+            [
+                "Jenesyx", "Bidkhori", "Website",
+                "Your friend", "Your family",
+                "is just kidding"
+            ],
+        typeSpeed: 120,
+        loop: true,
+        startDelay: 2200,
+        backSpeed: 60
+    });
+
+    $('.example-slider').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplaySpeed: 1500,
+        autoplay: true,
+        centerMode: false,
+        dots: true,
+    });
+}s
+
+function banana(t)
+{
+    if (t == 'in')
+    {
+        document.getElementById('circleone').style.background = "#ffff01"
+        document.getElementById('circletwo').style.background = "#3b4382"
+        return
+    }
+
+    else (t == 'out')
+    {
+        document.getElementById('circleone').style.background = "#3b4382"
+        document.getElementById('circletwo').style.background = "#ffff01"
+        return
+    }
+}
