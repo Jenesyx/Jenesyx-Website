5a7d16bf11ecd69b00568a35bbe5d9497d3d72b6
Ali Bidkhori <a.bidkhori2004@gmail.com>
2021-10-20 17:58:20 +0200

    1.1 Update - added new Codes

--- JS/Main.js
+++ JS/Main.js
@@ -21,21 +21,89 @@ function onload()
         centerMode: false,
         dots: true,
     });
-}s
+}
 
 function banana(t)
 {
     if (t == 'in')
     {
-        document.getElementById('circleone').style.background = "#ffff01"
-        document.getElementById('circletwo').style.background = "#3b4382"
+        document.getElementById('circleone').style.background = "#3b4382"
+        document.getElementById('circletwo').style.background = "#ffff01"
         return
     }
 
     else (t == 'out')
     {
-        document.getElementById('circleone').style.background = "#3b4382"
-        document.getElementById('circletwo').style.background = "#ffff01"
+        document.getElementById('circleone').style.background = "#ffff01"
+        document.getElementById('circletwo').style.background = "#3b4382"
+        return
+    }
+}
+
+function mooz(t)
+{
+    if (t == 'in')
+    {
+        document.getElementById('triangle1').style.opacity= "1"
+        document.getElementById('triangle1').style.transform= "rotate(65deg)"
+        return
+    }
+
+    else (t == 'out')
+    {
+        document.getElementById('triangle1').style.opacity= "0"
+        document.getElementById('triangle1').style.transform= "rotate(0deg)"
+        return
+    }
+}
+
+function sib(t)
+{
+    if (t == 'in')
+    {
+        document.getElementById('triangle2').style.opacity= "1"
+        document.getElementById('triangle2').style.transform= "rotate(65deg)"
+        return
+    }
+
+    else (t == 'out')
+    {
+        document.getElementById('triangle2').style.opacity= "0"
+        document.getElementById('triangle2').style.transform= "rotate(0deg)"
+        return
+    }
+}
+
+function golabi(t)
+{
+    if (t == 'in')
+    {
+        document.getElementById('triangle3').style.opacity= "1"
+        document.getElementById('triangle3').style.transform= "rotate(65deg)"
+        return
+    }
+
+    else (t == 'out')
+    {
+        document.getElementById('triangle3').style.opacity= "0"
+        document.getElementById('triangle3').style.transform= "rotate(0deg)"
+        return
+    }
+}
+
+function havij(t)
+{
+    if (t == 'in')
+    {
+        document.getElementById('triangle4').style.opacity= "1"
+        document.getElementById('triangle4').style.transform= "rotate(65deg)"
+        return
+    }
+
+    else (t == 'out')
+    {
+        document.getElementById('triangle4').style.opacity= "0"
+        document.getElementById('triangle4').style.transform= "rotate(0deg)"
         return
     }
 }
